001a2cc131c38fda6286e02ee7cd3692fdfe2eae
DESKTOP-MANSBU3\Admin <kenjiwinness@gmail.com>
2022-08-05 04:49:52 +0700

    fixed sim is not newly instanciated.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from class3 import RuneKnight
 from tools import SimsTool
 
 
 if __name__ == '__main__':
-    SimsTool.get_rune_mastery_best_status(level=102, is_transcended=True)
+    SimsTool.get_rune_mastery_best_status(level=105, is_transcended=True)
